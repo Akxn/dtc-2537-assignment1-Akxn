6b31b41547e16bb1a7e653d535b2b875e72d948a
Akxn <shen.yen112@gmail.com>
2022-05-02 21:43:01 -0700

    added random cards

--- public/code.js
+++ public/code.js
@@ -1,12 +1,22 @@
 to_add = ''
 
-function loadNineImages() {
+function processPokemonResponse(data) {
+    // console.log(data)
+    to_add += ` ${data.name} <div class="image_container"> <img src="${data.sprites.other["official-artwork"].front_default}"> </div>`
+
+}
+async function loadNineImages() {
     for (i = 1; i <= 9; i++) { // Nine times
         if (i % 3 == 1) {
             to_add += `<div class="images_group">`
         }
 
-        to_add += ` <div class="image_container"> <img src="https://picsum.photos/300/200?random=09832"> </div>`
+        x = Math.floor(Math.random() * 898) + 1
+        await $.ajax({
+            type: "GET",
+            url: `https://pokeapi.co/api/v2/pokemon/${x}/`,
+            success: processPokemonResponse
+        })
 
         if (i % 3 == 0) {
             to_add += `</div>`
