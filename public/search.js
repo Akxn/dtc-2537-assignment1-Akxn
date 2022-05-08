eac86e158f1779326c7af710ad5a1a6804d4c9c4
Akxn <shen.yen112@gmail.com>
2022-05-07 22:24:12 -0700

    added search js

--- public/search.js
+++ public/search.js
@@ -12,6 +12,16 @@ function processPokeResponse(data){
 
 }
 
+function processPokeRegion(data) {
+    console.log(data);
+    for (i = 0; i < data.results.length ; i++){
+
+            $("main").append("<div>" + data.results[i].name + "</div>")
+
+            // $("main").append(`<img src="${data.sprites.other["official-artwork"].front_default}" >`)
+    }
+}
+
 function display(type_){
     $("main").empty()
     type_g = type_
@@ -23,9 +33,18 @@ function display(type_){
             success: processPokeResponse
         })
     }
-
 }
 
+function displayRegion(start, end) {
+
+    $.ajax({
+        type: "get",
+        url: `https://pokeapi.co/api/v2/pokemon/?limit=${end}&offset=${start}`,
+        success: function(response) {
+            processPokeRegion(response)
+        }
+    })
+}
 
 function setup(){
 
@@ -35,9 +54,41 @@ function setup(){
         // alert($(this).attr("value"));
         poke_type = $("#poke_type option:selected").val();
         display(poke_type);
-
-
       })
+
+    $("#region").change(() => {
+        pokeregion = $("#region option:selected").val();
+        $('body').empty();
+        switch (pokeregion) {
+            case 'kanto':
+                displayRegion(1,151)
+                break;
+            case 'johto':
+                displayRegion(152,251)
+                break
+            case 'hoenn':
+                displayRegion(252,386)
+                break
+            case 'sinnoh':
+                displayRegion(387,494)
+                break
+            case 'unova':
+                displayRegion(495,649)
+                break
+            case 'kalos':
+                displayRegion(650,721)
+                break
+            case 'alola':
+                displayRegion(722,809)
+                break
+            case 'galar':
+                displayRegion(810,898)
+                break
+            default:
+                console.log('This shouldnt happen')
+                break
+        }
+    })
 }
 
 
